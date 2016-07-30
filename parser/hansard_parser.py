f33151ddb1770b70f757a7ef610451c62f126a4e
Danju <dvis5462@bitbucket.com>
2016-07-30 18:30:15 +1000

    added sys command

--- parser/hansard_parser.py
+++ parser/hansard_parser.py
@@ -1,9 +1,11 @@
 import xml.etree.ElementTree as ET
-
+import sys
+sys.path.insert(0, "..")
+from models import politician
 def parse_xml(xml):
     tree = ET.parse(xml)
     root = tree.getroot()
     print root.tag
 
 if __name__=="__main__":
-    parse_xml("sample.xml")
+    print "test"
