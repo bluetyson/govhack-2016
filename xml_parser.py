8bfa9a519cfc29ba31604532792464b6294c04fd
Danju <dvis5462@bitbucket.com>
2016-07-30 14:37:46 +1000

    added filez

---
+++ xml_parser.py
@@ -0,0 +1,9 @@
+import xml.etree.ElementTree as ET
+
+def parse_xml(xml):
+    tree = ET.parse(xml)
+    root = tree.getroot()
+    print root.tag
+
+if __name__=="__main__":
+    parse_xml("parliament_text.xml")
