ebf4bdf10c45e7dad3df16e7cfef09ca7252bcf6
Danju <dvis5462@bitbucket.com>
2016-07-30 16:16:11 +1000

    moved markov to separate package

---
+++ markov/markov.py
@@ -0,0 +1,9 @@
+import markovify
+
+from models import politician, state
+def main():
+    # [[state1, state2, state3, state4, state5], [state3, state2, state 4, state 1], [state5, state6, state4, state1]]
+    # markovify.chain([state1, state2, state3, state4], [state1, state2, state3])
+    pass
+if __name__ == "__main__":
+    main()
