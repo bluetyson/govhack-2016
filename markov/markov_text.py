ba64d4081c9a4a4f06b5e6200811c5f955267f85
Danju <dvis5462@bitbucket.com>
2016-07-31 00:39:19 +1000

    added markov text generatuin

--- markov/markov_text.py
+++ markov/markov_text.py
@@ -1,1 +1,21 @@
 import markovify
+import csv
+
+def markov_text(texts):
+    texts = texts[1:]
+    markov_dict = {}
+    for i in texts:
+        print i
+        model = markovify.Text(i[1].strip())
+        markov_dict[i[0]] = model.make_sentence()
+    return markov_dict
+
+
+def main():
+    with open("corpus.csv", "rb") as f:
+        reader = csv.reader(f, delimiter=' ', quotechar='|')
+        markov_text([row.replace(",", "|", 1).split("|") for row in f])
+
+
+if __name__ == "__main__":
+    main()
