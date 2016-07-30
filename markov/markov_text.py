327d3d11d4e06836a65082b2f9b27f70538b68c1
Danju <dvis5462@bitbucket.com>
2016-07-31 05:35:09 +1000

    added test print statements

--- markov/markov_text.py
+++ markov/markov_text.py
@@ -4,8 +4,10 @@ import csv
 def markov_text(texts):
     markov_dict = {}
     for i in texts:
-        # print texts[i][0][0]
-        markov_dict[i] = markovify.Text(texts[i][0][0].replace("(", "").replace(")", ""))
+        # print texts[i][0][0].replace("(", "").replace(")", "")
+        markov_dict[i] = markovify.Text(texts[i][0][0].replace("(", "").replace(")", "") + ".")
+        print markov_dict[i].make_sentence(tries=50000)
+    # print texts
 
     return markov_dict
 
