fc4f6c19f91f0dcbdea2c47695398bca7452e79e
Jalitha De Silva <jdes_10@hotmail.com>
2016-07-31 12:03:51 +1000

    Adding small animation

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 $( document ).ready(
-	//runApp
+	runApp
 )
 
 
@@ -17,21 +17,24 @@ var lelelel =
 [	
 	{
 		order: 0,
-		politicianId: 'R36',
+		politicianId: 'EZ5',
+		name: 'ToneDawgz',
 		type: 'talk',
-		content: 'I am the best person ever'
+		content: 'I am the best person ever lol, i stopped the botes.'
 	},	
 	{
 		order: 2,
-		politicianId: 'R36',
+		politicianId: 'EZ5',
+		name: 'ToneDawgz',
 		type: 'talk',
-		content: 'I totally am! o:'
+		content: 'lmao get fucked mal, I\'ve given you the answer u deserve lmfao - will take over soon xx'
 	},	
 	{
 		order: 1,
 		politicianId: '885',
+		name: 'Malcolm',
 		type: 'talk',
-		content: 'No you are not!'
+		content: 'No you are not! You couldn\'t even stay in power. You were a gross misuse of tax-payer money. We need to focus on jobs and growth, not coal and boats!'
 	}
 ]
 
@@ -39,17 +42,18 @@ var lelelel =
 
 
 function runApp(){
-	var source   = $("#entry-template").html();
+/*	var source   = $("#entry-template").html();
 	var template = Handlebars.compile(source);
 	var context = {politicianId: "21341", content: "This is my first post!"};
 	var html    = template(context);
 	var middleColumn = $('#middle-column');
 	middleColumn.append(html);
+*/
 	markovList.LoadMessages();
 
 	if (markovList.HasMessages())
 	{
-		setInterval(addNewMessage,500);
+		setInterval(addNewMessage, 3000);
 	}
 }	
 
@@ -62,12 +66,21 @@ function addNewMessage()
 	}
 
 	var message = markovList.GetNextMessage();	
-	var source   = $("#entry-template").html();
+
+	var templateName = "#message-template";
+	if (message.order % 2 == 1){
+		templateName = "#message-alt-template";
+	}
+
+	var source   = $(templateName).html();
 	var template = Handlebars.compile(source);
 	var context = message
 	var html    = template(context);
 	var middleColumn = $('#middle-column');
 	middleColumn.append(html);
+	var newMessageElement = $('#message-' + message.order);
+	newMessageElement.hide();
+	newMessageElement.slideDown(400);
 }
 
 function appendMessageDiv(message)
