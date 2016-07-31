4c1a9ab33f2b53ff1cfbd050a8f0a19fafe5132f
Danju <dvis5462@bitbucket.com>
2016-07-31 14:29:39 +1000

    added dynamic port

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
