4cd6acfdca6a10012590f5f358baff5c3c9f5703
Danju <dvis5462@bitbucket.com>
2016-07-31 16:57:26 +1000

    filtered empty content

--- app.py
+++ app.py
@@ -14,15 +14,17 @@ def get_markov():
     markov_stuff = HansardParser().parse(HANSARD_DATA)
     json_dict = []
     for i in markov_stuff:
-        tmp = {}
-        tmp["type"] = i.type
-        tmp["content"] = i.content
-        tmp["politician_id"] = i.state.politician_id
-        json_dict.append(tmp)
+        if i.content.strip():
+            tmp = {}
+            tmp["type"] = i.type
+            tmp["content"] = i.content
+            tmp["politician_id"] = i.state.politician_id
+            json_dict.append(tmp)
 
     return jsonify(json_dict)
 
 @app.route("/")
+
 def index():
     return render_template("index.html")
 
---
+++ parser/hansard_parser.py
@@ -0,0 +1,46 @@
+from models.politician import *
+from models.state import *
+from models.hansard_node import *
+from models.message import *
+from markov import markov_structure, markov_text
+import xml.etree.ElementTree as ET
+from collections import OrderedDict
+
+import random
+
+class HansardParser:
+    __excluded_ids = ["10000"]
+
+    def parse(self, path_to_xml):
+        xml = ET.parse(path_to_xml).getroot()
+        hansard_nodes = self.get_hansard_nodes(xml)
+        state_to_corpus = [x.get_state_and_corpus() for x in hansard_nodes]
+        states = [x[0] for x in state_to_corpus]
+        states_strings = [x.key for x in states]
+        generated_states = markov_structure.markov_chain(states_strings).walk(len(states_strings), states_strings[0])
+        generated_output = markov_text.markov_text(self.aggregate_state_to_corpus(state_to_corpus))
+        # print generated_output
+        for state in generated_states:
+            tmp = next((x for x in states if state == x.key), None)
+            gen_strings = []
+            model = generated_output[tmp]
+            message = Message(tmp, "".join([model.make_sentence(tries=5000) for i in xrange(random.randint(2, 20))]) )
+            if len(message.content.strip()):
+                yield message
+
+    def get_hansard_nodes(self, xml):
+        return [HansardNode(x) for x in xml.findall(".//talk.start/..")]
+
+    def aggregate_state_to_corpus(self, state_to_corpus):
+        result = dict()
+        for item in state_to_corpus:
+            state, corpus = item
+            if result.has_key(state):
+                result[state] += corpus
+            else:
+                result[state] = corpus
+        return result
+
+if __name__=="__main__":
+    parser = HansardParser()
+    parser.parse("sample.xml")
